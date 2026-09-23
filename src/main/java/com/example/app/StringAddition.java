cf06f6f7b00af343f343731eb933a9a7576378c6
fariyadali <fariyad123@gmail.com>
2026-09-23 17:23:33 +0530

    Add string addition main class

---
+++ src/main/java/com/example/app/StringAddition.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+public class StringAddition {
+    public static void main(String[] args) {
+        String first = "Hello, ";
+        String second = "World!";
+
+        System.out.println(first + second);
+    }
+}
